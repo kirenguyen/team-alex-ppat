d6be431c1bb95be7f02d2589e72d0cc2b7c7d7bb
McCoy Patino <39780829+mccoyp@users.noreply.github.com>
2019-12-03 17:47:19 -0500

    Update camerafeed.sh

--- camerafeed.sh
+++ camerafeed.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-raspidmx/pngview/pngview -b 0 -l 3 team-alex-ppat-master/overlays/overlay.png & raspivid -t 0
+raspidmx/pngview/pngview -b 0 -l 3 team-alex-ppat/overlays/overlay.png & raspivid -t 0
